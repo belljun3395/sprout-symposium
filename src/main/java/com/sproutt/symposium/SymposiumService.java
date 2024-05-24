01d3a8fed34c6b9595351f2038a4df08e2624525
belljun3395 <195850@jnu.ac.kr>
2024-05-24 14:47:45 +0900

    SymposiumService 구현

---
+++ src/main/java/com/sproutt/symposium/SymposiumService.java
@@ -0,0 +1,46 @@
+package com.sproutt.symposium;
+
+import com.sproutt.symposium.lock.LocalLockService;
+import com.sproutt.symposium.lock.LockService;
+import java.time.LocalDateTime;
+import java.util.concurrent.atomic.AtomicLong;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SymposiumService {
+
+	private final Logger log = LoggerFactory.getLogger(SymposiumService.class);
+	private final LockService lockService;
+	private final CountDownService countDownService;
+	private AtomicLong atomicUserId = new AtomicLong(0L);
+
+	public SymposiumService(LocalLockService lockService, CountDownService countDownService) {
+		this.lockService = lockService;
+		this.countDownService = countDownService;
+	}
+
+	public String execute() {
+		Long targetId = 1L;
+		Long userId = atomicUserId.incrementAndGet();
+		LocalDateTime now = LocalDateTime.now();
+		log.info("[{}] User {} is trying to execute", now, userId);
+
+		while (!lockService.lock(targetId, userId)) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		try {
+			countDownService.countDown(userId + " is executing");
+		} finally {
+			lockService.unlock(targetId, userId);
+		}
+
+		return "[" + now + "] User " + userId + " executed successfully";
+	}
+}
