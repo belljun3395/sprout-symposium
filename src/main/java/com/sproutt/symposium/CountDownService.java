49e3c9b6babaaeec6a2ac9dc30ad04fd00cd5333
belljun3395 <195850@jnu.ac.kr>
2024-05-24 14:46:23 +0900

    CountDownService 생성

---
+++ src/main/java/com/sproutt/symposium/CountDownService.java
@@ -0,0 +1,22 @@
+package com.sproutt.symposium;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CountDownService {
+
+	private final Logger log = LoggerFactory.getLogger(CountDownService.class);
+	private AtomicInteger countDown = new AtomicInteger(1000);
+
+	public void countDown(String message) {
+		if (countDown.getAndDecrement() > 0) {
+			log.info("Counting down to {} by UserId {}", countDown, message);
+		} else {
+			log.error("CountDown is already 0, {}", message);
+			throw new RuntimeException("CountDown is already 0");
+		}
+	}
+}
