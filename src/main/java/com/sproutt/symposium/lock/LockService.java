770679529c5504ee0c468dcbe5a52e2365cfdbd0
belljun3395 <195850@jnu.ac.kr>
2024-05-27 11:00:03 +0900

    LockService 메서드의 주석을 추가합니다

--- src/main/java/com/sproutt/symposium/lock/LockService.java
+++ src/main/java/com/sproutt/symposium/lock/LockService.java
@@ -2,7 +2,13 @@ package com.sproutt.symposium.lock;
 
 public interface LockService {
 
+	/**
+	 * lockId에 대한 lock을 userId에게 할당한다.
+	 *
+	 * @return lock 할당 성공 여부
+	 */
 	boolean lock(Long id, Long userId);
 
+	/** lockId에 대한 lock을 해제한다. */
 	void unlock(Long id, Long userId);
 }
