1ba4e37e34031890cdedf0eeefc66c893b0dcf5c
belljun3395 <195850@jnu.ac.kr>
2024-05-27 15:18:03 +0900

    RabbitMQ 관련 설정을 추가

--- src/main/java/com/sproutt/symposium/SymposiumApplication.java
+++ src/main/java/com/sproutt/symposium/SymposiumApplication.java
@@ -1,14 +1,49 @@
 package com.sproutt.symposium;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableRabbit
 @SpringBootApplication
 public class SymposiumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SymposiumApplication.class, args);
+	}
+
+	private static final String X_MESSAGE_TTL_KEY = "x-message-ttl";
+	private static final Long X_MESSAGE_TTL = 1000 * 60 * 30L;
+
+	@Bean
+	MessageConverter ampqMessageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
+
+	@Bean
+	DirectExchange waitingDirect() {
+		return new DirectExchange("direct.waiting");
+	}
+
+	@Bean
+	Queue waitingQueue() {
+		return QueueBuilder.durable("queue.waiting")
+				.withArgument(X_MESSAGE_TTL_KEY, X_MESSAGE_TTL)
+				.build();
+	}
+
+	@Bean
+	Binding waitingQBinding() {
+		return BindingBuilder.bind(waitingQueue()).to(waitingDirect()).with("direct.waiting");
 	}
 }
