c6672c59550cfe612f837be966fcc8025fc3f8d9
belljun3395 <195850@jnu.ac.kr>
2024-05-27 13:07:29 +0900

    로그를 삭제하고 예외만 발생하도록 수정

--- src/main/java/com/sproutt/symposium/CountDownService.java
+++ src/main/java/com/sproutt/symposium/CountDownService.java
@@ -1,6 +1,7 @@
 package com.sproutt.symposium;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import org.apache.logging.log4j.message.StringFormattedMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -15,8 +16,9 @@ public class CountDownService {
 		if (countDown.getAndDecrement() > 0) {
 			log.info("Counting down to {} by UserId {}", countDown, message);
 		} else {
-			log.error("CountDown is already 0, {}", message);
-			throw new RuntimeException("CountDown is already 0");
+			StringFormattedMessage formattedMessage =
+					new StringFormattedMessage("CountDown is already 0, %s", message);
+			throw new RuntimeException(formattedMessage.getFormattedMessage());
 		}
 	}
 }
